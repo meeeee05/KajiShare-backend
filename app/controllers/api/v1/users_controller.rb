ffcd6d5f98f6e342c6cdd413850ab18e3579ca61
meeeee05 <stmi.eeeee05@gmail.com>
2025-11-12 23:21:34 +0900

    User Controller設定

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,30 +1,48 @@
-class Api::V1::UsersController < Api::V1::BaseController
-  before_action :set_user, only: [:show, :update]
+module Api
+  module V1
+    class UsersController < ApplicationController
+      before_action :set_user, only: [:show, :update, :destroy]
 
-  def index
-    users = User.all
-    render_success({ users: users })
-  end
+      def index
+        users = User.all
+        render json: users
+      end
 
-  def show
-    render_success({ user: @user })
-  end
+      def show
+        render json: @user
+      end
 
-  def update
-    if @user.update(user_params)
-      render_success({ user: @user }, "User updated successfully")
-    else
-      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+      def create
+        user = User.new(user_params)
+        if user.save
+          render json: user, status: :created
+        else
+          render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      def update
+        if @user.update(user_params)
+          render json: @user
+        else
+          render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        @user.destroy
+        head :no_content
+      end
+
+      private
+
+      def set_user
+        @user = User.find(params[:id])
+      end
+
+      def user_params
+        params.require(:user).permit(:google_sub, :name, :email, :picture, :account_type)
+      end
     end
-  end
-
-  private
-
-  def set_user
-    @user = User.find(params[:id])
-  end
-
-  def user_params
-    params.require(:user).permit(:name, :email, :picture)
   end
 end
