b6e5252a6a6723786d5f9dd7b26af1a1f86775b3
meeeee05 <stmi.eeeee05@gmail.com>
2025-11-13 23:44:23 +0900

    membrships_controller作成

--- app/controllers/api/v1/memberships_controller.rb
+++ app/controllers/api/v1/memberships_controller.rb
@@ -1,2 +1,48 @@
-class Api::V1::MembershipsController < ApplicationController
+module Api
+  module V1
+    class MembershipsController < ApplicationController
+      before_action :set_membership, only: [:show, :update, :destroy]
+
+      def index
+        memberships = Membership.all
+        render json: memberships
+      end
+
+      def show
+        render json: @membership
+      end
+
+      def create
+        membership = Membership.new(membership_params)
+        if membership.save
+          render json: membership, status: :created
+        else
+          render json: { errors: membership.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      def update
+        if @membership.update(membership_params)
+          render json: @membership
+        else
+          render json: { errors: @membership.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        @membership.destroy
+        head :no_content
+      end
+
+      private
+
+      def set_membership
+        @membership = Membership.find(params[:id])
+      end
+
+      def membership_params
+        params.require(:membership).permit(:user_id, :group_id, :role, :workload_ratio, :active)
+      end
+    end
+  end
 end
