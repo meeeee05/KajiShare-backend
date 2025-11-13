aebcffe64db27ce090389f6b7057ba5ef68af473
meeeee05 <stmi.eeeee05@gmail.com>
2025-11-13 15:38:58 +0900

    score修正

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -4,7 +4,8 @@ class Evaluation < ApplicationRecord
   belongs_to :evaluator, class_name: 'User', foreign_key: 'evaluator_id'
 
   #コメントを持つ場合のバリデーション
-  validates :feedback, length: { maximum: 500 }, allow_blank: true
+  validates :score, length: { maximum: 500 }, allow_blank: true
+  #validates :feedback, length: { maximum: 500 }, allow_blank: true
 
   #評価スコア（例: 1〜5）
   validates :score, inclusion: { in: 1..5 }
