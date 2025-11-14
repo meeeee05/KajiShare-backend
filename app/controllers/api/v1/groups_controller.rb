fea0d3c96bc0a38a344d4b1ee4ee6b60c759ee6b
meeeee05 <stmi.eeeee05@gmail.com>
2025-11-14 15:44:12 +0900

    groups_controller作成

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -1,2 +1,48 @@
 class Api::V1::GroupsController < ApplicationController
+  # GET /api/v1/groups
+  def index
+    groups = Group.all
+    render json: groups
+  end
+
+  # GET /api/v1/groups/:id
+  def show
+    group = Group.find(params[:id])
+    render json: group
+  end
+
+  # POST /api/v1/groups
+  def create
+    group = Group.new(group_params)
+
+    if group.save
+      render json: group, status: :created
+    else
+      render json: { errors: group.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /api/v1/groups/:id
+  def update
+    group = Group.find(params[:id])
+
+    if group.update(group_params)
+      render json: group
+    else
+      render json: { errors: group.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /api/v1/groups/:id
+  def destroy
+    group = Group.find(params[:id])
+    group.destroy
+    head :no_content
+  end
+
+  private
+
+  def group_params
+    params.require(:group).andpermit(:name, :share_key, :assign_mode, :balance_type, :active)
+  end
 end
