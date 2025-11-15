f12e1f577b7ff7ea0f5affa88f848acfe22ab4b3
meeeee05 <stmi.eeeee05@gmail.com>
2025-11-15 23:52:36 +0900

    コメント修正

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,6 +3,7 @@ module Api
     class UsersController < ApplicationController
       before_action :set_user, only: [:show, :update, :destroy]
 
+      #User一覧取得
       def index
         users = User.all
         render json: users
