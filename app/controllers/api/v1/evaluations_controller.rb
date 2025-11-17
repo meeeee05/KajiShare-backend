025dc46bda73b65a56d878de04de49bef1c6defa
meeeee05 <stmi.eeeee05@gmail.com>
2025-11-17 23:25:16 +0900

    evaluations_controller作成

--- app/controllers/api/v1/evaluations_controller.rb
+++ app/controllers/api/v1/evaluations_controller.rb
@@ -1,2 +1,53 @@
-class Api::V1::EvaluationsController < ApplicationController
+module Api
+  module V1
+    class EvaluationsController < ApplicationController
+      before_action :set_evaluation, only: [:show, :update]
+
+      # GET /api/v1/evaluations
+      def index
+        evaluations = Evaluation.all
+        render json: evaluations
+      end
+
+      # GET /api/v1/evaluations/:id
+      def show
+        render json: @evaluation
+      end
+
+      # POST /api/v1/evaluations
+      def create
+        evaluation = Evaluation.new(evaluation_params)
+
+        if evaluation.save
+          render json: evaluation, status: :created
+        else
+          render json: { errors: evaluation.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      # PATCH/PUT /api/v1/evaluations/:id
+      def update
+        if @evaluation.update(evaluation_params)
+          render json: @evaluation
+        else
+          render json: { errors: @evaluation.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def set_evaluation
+        @evaluation = Evaluation.find(params[:id])
+      end
+
+      #カラムに合わせたpermit
+      #assignment_id
+      #evaluator_id
+      #score
+      #comments
+      def evaluation_params
+        params.require(:evaluation).permit(:assignment_id, :evaluator_id, :score, :feedback)
+      end
+    end
+  end
 end
