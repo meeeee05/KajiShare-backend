c3335985848b01ab6297631f5a911113e1f94a7d
meeeee05 <stmi.eeeee05@gmail.com>
2025-11-30 17:29:36 +0900

    EvaluationsController権限制御

--- app/controllers/api/v1/evaluations_controller.rb
+++ app/controllers/api/v1/evaluations_controller.rb
@@ -75,7 +75,7 @@ module Api
         params.require(:evaluation).permit(:assignment_id, :evaluator_id, :score, :feedback)
       end
 
-      # Member権限チェック：指定されたグループのメンバー（admin or member）以上の権限のみ操作可能
+      # Member権限チェック：指定されたグループのmember以上の権限のみ操作可能
       def check_member_permission
         # group_idの取得（アクションごとに取得）
         case action_name
