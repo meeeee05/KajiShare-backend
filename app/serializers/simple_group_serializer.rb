88ed996477e603eee57f6e328d4bf2412e36210d
meeeee05 <stmi.eeeee05@gmail.com>
2025-12-07 23:19:20 +0900

    リファクタリング

--- app/serializers/simple_group_serializer.rb
+++ app/serializers/simple_group_serializer.rb
@@ -1,6 +1,7 @@
 class SimpleGroupSerializer < ActiveModel::Serializer
   attributes :id, :name, :share_key, :assign_mode, :balance_type, :active, :members_count
 
+  
   def members_count
     object.memberships.where(active: true).count
   end
