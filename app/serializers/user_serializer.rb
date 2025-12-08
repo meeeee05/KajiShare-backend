289c89877f3753092637094c7d3a8d96b9fec4b7
meeeee05 <stmi.eeeee05@gmail.com>
2025-12-08 23:19:07 +0900

    UserSerializer整理

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,10 +1,10 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :picture, :account_type
 
-  # 関連データの包含（オプション）
+  # 条件付きでメンバーシップ（グループ参加情報）を取得
   has_many :memberships, serializer: MembershipSerializer, if: :include_memberships?
 
-  # カスタム　属性
+  # カスタム属性
   attribute :groups_count
   attribute :active_groups
 
@@ -20,8 +20,9 @@ class UserSerializer < ActiveModel::Serializer
     groups.map { |group| SimpleGroupSerializer.new(group).as_json }
   end
 
+  # メンバーシップの情報を含めるかどうか判定
   def include_memberships?
-    # コンテキストで制御可能
+    # コンテキストで制御
     instance_options[:include_memberships] || false
   end
 end
