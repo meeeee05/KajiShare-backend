72500fe775318bad795b9a16b595ac9b20d2857b
meeeee05 <stmi.eeeee05@gmail.com>
2025-12-18 23:19:06 +0900

    TaskModelにバリデーション追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,12 @@
 class Task < ApplicationRecord
-  
-  #model関連付け
+  # model関連付け
   belongs_to :group
   has_many :assignments, dependent: :destroy
+
+  # バリデーション
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :description, length: { maximum: 500 }, allow_blank: true
+  validates :point,
+            presence: true,
+            numericality: { only_integer: true, greater_than: 0 }
 end
