460696f1d7a5f9286899609bdb5e0cc8635a1821
meeeee05 <stmi.eeeee05@gmail.com>
2025-12-20 13:19:58 +0900

    MembershipModelバリデーション追加

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -5,5 +5,18 @@ class Membership < ApplicationRecord
   has_many :assignments, dependent: :destroy
   has_many :evaluations, through: :assignments, dependent: :destroy
 
-  validates :role, presence: true
+  #バリデーション（フォーマットチェック）
+  validates :role, presence: true, inclusion: { in: %w[admin member] }
+
+  validates :user_id, uniqueness: {
+    scope: :group_id,
+    message: "はすでにこのグループに参加しています"
+  }
+
+  validates :workload_ratio,
+            numericality: {
+              greater_than: 0,
+              less_than_or_equal_to: 100
+            },
+            allow_nil: true
 end
