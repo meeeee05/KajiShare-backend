bb096dd2c24321fd204411955000d965e9bd34c3
meeeee05 <stmi.eeeee05@gmail.com>
2025-12-22 22:56:26 +0900

    Evaluationモデルバリデーション追加

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -1,12 +1,22 @@
 class Evaluation < ApplicationRecord
-  #model関連付け
+  # model関連付け
   belongs_to :assignment
-  belongs_to :evaluator, class_name: 'User', foreign_key: 'evaluator_id'
+  belongs_to :evaluator,
+             class_name: 'User',
+             foreign_key: 'evaluator_id'
 
-  #コメントを持つ場合のバリデーション
-  validates :score, length: { maximum: 500 }, allow_blank: true
-  #validates :feedback, length: { maximum: 500 }, allow_blank: true
+  # バリデーション
+  # 評価スコア（必須・1〜5）
+  validates :score,
+            presence: true,
+            numericality: {
+              only_integer: true,
+              greater_than_or_equal_to: 1,
+              less_than_or_equal_to: 5
+            }
 
-  #評価スコア（例: 1〜5）
-  validates :score, inclusion: { in: 1..5 }
+  # コメント（任意・最大500文字）
+  validates :feedback,
+            length: { maximum: 500 },
+            allow_blank: true
 end
