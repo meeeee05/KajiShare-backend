d6e2f7ce4a236d5abbe5618f775e8756b2fbe494
meeeee05 <stmi.eeeee05@gmail.com>
2026-01-07 22:41:53 +0900

    リファクタリング

--- app/serializers/membership_serializer.rb
+++ app/serializers/membership_serializer.rb
@@ -5,24 +5,29 @@ class MembershipSerializer < ActiveModel::Serializer
   belongs_to :user, serializer: SimpleUserSerializer
   belongs_to :group, serializer: SimpleGroupSerializer
 
+  # ユーザー名取得
   def user_name
     user&.name
   end
 
+  # グループ名取得
   def group_name
     group&.name
   end
 
+  # メンバーに割り当てられた課題の総数をカウント
   def assignments_count
     user_assignments.count
   end
-
+ 
+  # メンバーが完了した課題の数をカウント
   def completed_assignments_count
     user_assignments.completed.count
   end
 
   private
 
+  # ユーザーに割り当てられた課題を取得
   def user_assignments
     @user_assignments ||= object.assignments
   end
