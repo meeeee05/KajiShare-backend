15650b210ac36456d8ed2970dbb47f9f7dccaa1d
meeeee05 <stmi.eeeee05@gmail.com>
2026-01-28 23:27:02 +0900

    assighmentAPIテスト項目修正

--- spec/requests/api/v1/assignments_spec.rb
+++ spec/requests/api/v1/assignments_spec.rb
@@ -1,13 +1,16 @@
 # frozen_string_literal: true
-
 require 'rails_helper'
+
+def json
+  JSON.parse(response.body)
+end
 
 RSpec.describe "Api::V1::Assignments", type: :request do
   let!(:group) { create(:group) }
   let!(:user) { create(:user) }
   let!(:admin_user) { create(:user) }
-  let!(:member_membership) { create(:membership, user: user, group: group, role: 'member', active: true) }
-  let!(:admin_membership) { create(:membership, user: admin_user, group: group, role: 'admin', active: true) }
+  let!(:member_membership) { create(:membership, user: user, group: group, role: 'member', active: true, workload_ratio: 100) }
+  # admin_membershipは必要なテスト内で個別に作成
   let!(:task) { create(:task, group: group) }
   let!(:assignment) { create(:assignment, task: task, membership: member_membership) }
   let(:headers) { { "Authorization" => "Bearer valid-token" } }
@@ -18,135 +21,208 @@ RSpec.describe "Api::V1::Assignments", type: :request do
   end
 
   describe "GET /api/v1/tasks/:task_id/assignments" do
-    it "returns assignments for the task if user is member" do
-      get "/api/v1/tasks/#{task.id}/assignments", headers: headers
-      expect(response).to have_http_status(:ok)
-      json = JSON.parse(response.body)
-      expect(json["data"]).to be_an(Array)
-      expect(json["data"].first["id"].to_i).to eq(assignment.id)
+    subject { get "/api/v1/tasks/#{task_id}/assignments", headers: headers }
+
+    context "when user is member" do
+      let(:task_id) { task.id }
+      it do
+        subject
+        expect(response).to have_http_status(:ok)
+        expect(json["data"]).to be_an(Array)
+        expect(json["data"].first["id"].to_i).to eq(assignment.id)
+      end
     end
-    it "returns forbidden if user is not group member" do
-      other_group = create(:group)
-      other_task = create(:task, group: other_group)
-      get "/api/v1/tasks/#{other_task.id}/assignments", headers: headers
-      expect(response).to have_http_status(:forbidden)
+
+    context "when user is not group member" do
+      let(:other_group) { create(:group) }
+      let(:task_id) { create(:task, group: other_group).id }
+      it do
+        subject
+        expect(response).to have_http_status(:forbidden)
+      end
     end
-    it "returns 404 if task does not exist" do
-      get "/api/v1/tasks/99999999/assignments", headers: headers
-      expect(response).to have_http_status(:not_found)
+
+    context "when task does not exist" do
+      let(:task_id) { 99999999 }
+      it do
+        subject
+        expect(response).to have_http_status(:not_found)
+      end
     end
-    it "returns 404 with invalid id format" do
-      get "/api/v1/tasks/invalid_id/assignments", headers: headers
-      expect(response).to have_http_status(:not_found)
+
+    context "when id format is invalid" do
+      let(:task_id) { 'invalid_id' }
+      it do
+        subject
+        expect(response).to have_http_status(:not_found)
+      end
     end
   end
 
   describe "GET /api/v1/assignments/:id" do
-    it "shows assignment if user is group member" do
-      get "/api/v1/assignments/#{assignment.id}", headers: headers
-      expect(response).to have_http_status(:ok)
-      json = JSON.parse(response.body)
-      expect(json["data"]["id"].to_i).to eq(assignment.id)
+    subject { get "/api/v1/assignments/#{assignment_id}", headers: headers }
+
+    context "when user is group member" do
+      let(:assignment_id) { assignment.id }
+      it do
+        subject
+        expect(response).to have_http_status(:ok)
+        expect(json["data"]["id"].to_i).to eq(assignment.id)
+      end
     end
-    it "returns 404 if assignment does not exist" do
-      get "/api/v1/assignments/99999999", headers: headers
-      expect(response).to have_http_status(:not_found)
+    context "when assignment does not exist" do
+      let(:assignment_id) { 99999999 }
+      it do
+        subject
+        expect(response).to have_http_status(:not_found)
+      end
     end
-    it "returns 404 with invalid id format" do
-      get "/api/v1/assignments/invalid_id", headers: headers
-      expect(response).to have_http_status(:not_found)
+    context "when id format is invalid" do
+      let(:assignment_id) { 'invalid_id' }
+      it do
+        subject
+        expect(response).to have_http_status(:not_found)
+      end
     end
-    it "returns forbidden if user is not group member" do
-      other_group = create(:group)
-      other_task = create(:task, group: other_group)
-      other_membership = create(:membership, user: create(:user), group: other_group, role: 'member', active: true)
-      other_assignment = create(:assignment, task: other_task, membership: other_membership)
-      get "/api/v1/assignments/#{other_assignment.id}", headers: headers
-      expect(response).to have_http_status(:forbidden)
+    context "when user is not group member" do
+      let(:other_group) { create(:group) }
+      let(:other_task) { create(:task, group: other_group) }
+      let(:other_membership) { create(:membership, user: create(:user), group: other_group, role: 'member', active: true, workload_ratio: 100) }
+      let(:assignment_id) { create(:assignment, task: other_task, membership: other_membership).id }
+      it do
+        subject
+        expect(response).to have_http_status(:forbidden)
+      end
     end
   end
 
   describe "POST /api/v1/tasks/:task_id/assignments" do
-    let(:valid_params) do
-      { assignment: { due_date: Date.tomorrow, comment: "test" } }
+    let(:valid_params) { { assignment: { due_date: Date.tomorrow, comment: "test" } } }
+    subject { post "/api/v1/tasks/#{task_id}/assignments", params: params, headers: headers }
+
+    context "when user is member" do
+      let(:task_id) { task.id }
+      let(:params) { valid_params }
+      it "creates assignment" do
+        another_user = create(:user)
+        member_membership.destroy # 既存のmembershipを削除し、合計100を担保
+        create(:membership, user: another_user, group: group, role: 'member', active: true, workload_ratio: 100)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(another_user)
+        expect { post "/api/v1/tasks/#{task.id}/assignments", params: valid_params, headers: headers }.to change(Assignment, :count).by(1)
+        expect(response).to have_http_status(:created)
+        expect(json["data"]["attributes"]["comment"]).to eq("test")
+      end
     end
-    it "creates assignment if user is member" do
-      another_user = create(:user)
-      another_membership = create(:membership, user: another_user, group: group, role: 'member', active: true)
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(another_user)
-      expect {
-        post "/api/v1/tasks/#{task.id}/assignments", params: valid_params, headers: headers
-      }.to change(Assignment, :count).by(1)
-      expect(response).to have_http_status(:created)
-      json = JSON.parse(response.body)
-      expect(json["data"]["attributes"]["comment"]).to eq("test")
+
+    context "when params invalid" do
+      let(:task_id) { task.id }
+      let(:params) { { assignment: { due_date: nil } } }
+      it do
+        subject
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(json["errors"]).to be_present
+      end
     end
-    it "returns 422 if params invalid" do
-      post "/api/v1/tasks/#{task.id}/assignments", params: { assignment: { due_date: nil } }, headers: headers
-      expect(response).to have_http_status(:unprocessable_entity)
-      json = JSON.parse(response.body)
-      expect(json["errors"]).to be_present
+
+    context "when user is not group member" do
+      let(:other_group) { create(:group) }
+      let(:task_id) { create(:task, group: other_group).id }
+      let(:params) { { assignment: { due_date: Date.tomorrow } } }
+      it do
+        subject
+        expect(response).to have_http_status(:forbidden)
+      end
     end
-    it "returns forbidden if user is not group member" do
-      other_group = create(:group)
-      other_task = create(:task, group: other_group)
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-      post "/api/v1/tasks/#{other_task.id}/assignments", params: { assignment: { due_date: Date.tomorrow } }, headers: headers
-      expect(response).to have_http_status(:forbidden)
+
+    context "when task does not exist" do
+      let(:task_id) { 99999999 }
+      let(:params) { { assignment: { due_date: Date.tomorrow } } }
+      it do
+        subject
+        expect(response).to have_http_status(:not_found)
+      end
     end
-    it "returns 404 if task does not exist" do
-      post "/api/v1/tasks/99999999/assignments", params: { assignment: { due_date: Date.tomorrow } }, headers: headers
-      expect(response).to have_http_status(:not_found)
-    end
-    it "returns 404 with invalid id format" do
-      post "/api/v1/tasks/invalid_id/assignments", params: { assignment: { due_date: Date.tomorrow } }, headers: headers
-      expect(response).to have_http_status(:not_found)
+
+    context "when id format is invalid" do
+      let(:task_id) { 'invalid_id' }
+      let(:params) { { assignment: { due_date: Date.tomorrow } } }
+      it do
+        subject
+        expect(response).to have_http_status(:not_found)
+      end
     end
   end
 
   describe "PATCH /api/v1/assignments/:id" do
     let(:update_params) { { assignment: { comment: "updated" } } }
-    it "updates assignment if user is member" do
-      patch "/api/v1/assignments/#{assignment.id}", params: update_params, headers: headers
-      expect(response).to have_http_status(:ok)
-      json = JSON.parse(response.body)
-      expect(json["data"]["attributes"]["comment"]).to eq("updated")
+    subject { patch "/api/v1/assignments/#{assignment_id}", params: params, headers: headers }
+
+    context "when user is member" do
+      let(:assignment_id) { assignment.id }
+      let(:params) { update_params }
+      it do
+        subject
+        expect(response).to have_http_status(:ok)
+        expect(json["data"]["attributes"]["comment"]).to eq("updated")
+      end
     end
-    it "returns 404 if assignment does not exist" do
-      patch "/api/v1/assignments/99999999", params: { assignment: { comment: "x" } }, headers: headers
-      expect(response).to have_http_status(:not_found)
+    context "when assignment does not exist" do
+      let(:assignment_id) { 99999999 }
+      let(:params) { { assignment: { comment: "x" } } }
+      it do
+        subject
+        expect(response).to have_http_status(:not_found)
+      end
     end
-    it "returns forbidden if user is not group member" do
-      other_group = create(:group)
-      other_task = create(:task, group: other_group)
-      other_membership = create(:membership, user: create(:user), group: other_group, role: 'member', active: true)
-      other_assignment = create(:assignment, task: other_task, membership: other_membership)
-      patch "/api/v1/assignments/#{other_assignment.id}", params: { assignment: { comment: "x" } }, headers: headers
-      expect(response).to have_http_status(:forbidden)
+    context "when user is not group member" do
+      let(:other_group) { create(:group) }
+      let(:other_task) { create(:task, group: other_group) }
+      let(:other_membership) { create(:membership, user: create(:user), group: other_group, role: 'member', active: true, workload_ratio: 100) }
+      let(:assignment_id) { create(:assignment, task: other_task, membership: other_membership).id }
+      let(:params) { { assignment: { comment: "x" } } }
+      it do
+        subject
+        expect(response).to have_http_status(:forbidden)
+      end
     end
   end
 
   describe "DELETE /api/v1/assignments/:id" do
-    it "deletes assignment if user is admin" do
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin_user)
-      assignment_to_delete = create(:assignment, task: task, membership: admin_membership)
-      expect {
-        delete "/api/v1/assignments/#{assignment_to_delete.id}", headers: headers
-      }.to change(Assignment, :count).by(-1)
-      expect(response).to have_http_status(:ok)
-      json = JSON.parse(response.body)
-      expect(json["message"]).to include("successfully deleted")
+    subject { delete "/api/v1/assignments/#{assignment_id}", headers: headers }
+
+    context "when user is admin" do
+      let!(:admin_membership) do
+        member_membership.destroy # 既存のmembershipを削除し、合計100を担保
+        create(:membership, user: admin_user, group: group, role: 'admin', active: true, workload_ratio: 100)
+      end
+      let!(:admin_assignment) { create(:assignment, task: task, membership: admin_membership) }
+      let(:assignment_id) { admin_assignment.id }
+      before { allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin_user) }
+      it do
+        expect { subject }.to change(Assignment, :count).by(-1)
+        expect(response).to have_http_status(:ok)
+        expect(json["message"]).to include("successfully deleted")
+      end
     end
-    it "returns forbidden if not admin" do
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-      assignment_to_delete = create(:assignment, task: task, membership: admin_membership)
-      delete "/api/v1/assignments/#{assignment_to_delete.id}", headers: headers
-      expect(response).to have_http_status(:forbidden)
+    context "when not admin" do
+      let!(:admin_membership) do
+        member_membership.destroy # 既存のmembershipを削除し、合計100を担保
+        create(:membership, user: admin_user, group: group, role: 'admin', active: true, workload_ratio: 100)
+      end
+      let(:assignment_id) { create(:assignment, task: task, membership: admin_membership).id }
+      before { allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user) }
+      it do
+        subject
+        expect(response).to have_http_status(:forbidden)
+      end
     end
-    it "returns 404 if assignment does not exist" do
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin_user)
-      delete "/api/v1/assignments/99999999", headers: headers
-      expect(response).to have_http_status(:not_found)
+    context "when assignment does not exist" do
+      let(:assignment_id) { 99999999 }
+      before { allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin_user) }
+      it do
+        subject
+        expect(response).to have_http_status(:not_found)
+      end
     end
   end
 end
--- spec/requests/api/v1/tasks_spec.rb
+++ spec/requests/api/v1/tasks_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe "Api::V1::Tasks", type: :request do
   let(:group) { create(:group) }
   let(:headers) { { "Authorization" => "Bearer valid-token" } }
   let(:json) { JSON.parse(response.body) }
-
   let!(:admin_membership) { create(:membership, user: admin_user, group: group, role: 'admin', active: true) }
   let!(:member_membership) { create(:membership, user: user, group: group, role: 'member', active: true) }
   let!(:task) { create(:task, group: group) }
@@ -23,6 +22,7 @@ RSpec.describe "Api::V1::Tasks", type: :request do
     it { expect(response).to have_http_status(:forbidden) }
   end
 
+  # 共通処理：リソースが見つからない場合のレスポンス
   shared_examples 'not_found' do |msg|
     it do
       expect(response).to have_http_status(:not_found)
@@ -31,6 +31,8 @@ RSpec.describe "Api::V1::Tasks", type: :request do
   end
 
   describe "GET /api/v1/groups/:group_id/tasks" do
+
+    # 正常系：グループメンバーがタスクを取得
     context "as group member" do
       it "returns tasks for group" do
         get "/api/v1/groups/#{group.id}/tasks", headers: headers
@@ -41,6 +43,7 @@ RSpec.describe "Api::V1::Tasks", type: :request do
       end
     end
 
+    # 異常系：非メンバーがアクセス
     context "as non-member" do
       before { allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(create(:user)) }
       it_behaves_like 'forbidden' do
@@ -50,6 +53,7 @@ RSpec.describe "Api::V1::Tasks", type: :request do
   end
 
   describe "GET /api/v1/tasks/:id" do
+    # 正常系：グループメンバーのタスクを表示
     context "as group member" do
       it "shows task" do
         get "/api/v1/tasks/#{task.id}", headers: headers
@@ -59,6 +63,7 @@ RSpec.describe "Api::V1::Tasks", type: :request do
       end
     end
 
+    # 異常系：非メンバーがタスクにアクセス
     context "as non-member" do
       before { allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(create(:user)) }
       it_behaves_like 'forbidden' do
@@ -69,7 +74,7 @@ RSpec.describe "Api::V1::Tasks", type: :request do
 
   describe "POST /api/v1/groups/:group_id/tasks" do
     let(:valid_params) { { task: { name: "New Task", description: "desc", point: 10 } } }
-
+    # 正常系：グループメンバーがタスクを作成
     context "as group member" do
       it "creates task" do
         expect {
@@ -79,12 +84,14 @@ RSpec.describe "Api::V1::Tasks", type: :request do
         expect(json["data"]["attributes"]["name"]).to eq("New Task")
       end
 
+      # 異常系：無効なパラメータでタスク作成
       it "returns error if params invalid" do
         post "/api/v1/groups/#{group.id}/tasks", params: { task: { name: "" } }, headers: headers
         expect(response).to have_http_status(:unprocessable_content)
       end
     end
 
+    # 異常系：非メンバーがタスク作成
     context "as non-member" do
       before { allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(create(:user)) }
       it_behaves_like 'forbidden' do
@@ -95,7 +102,7 @@ RSpec.describe "Api::V1::Tasks", type: :request do
 
   describe "PATCH /api/v1/tasks/:id" do
     let(:update_params) { { task: { name: "Updated Task" } } }
-
+    # 正常系：グループメンバーがタスクを更新
     context "as group member" do
       it "updates task" do
         task = create(:task, group: group)
@@ -106,6 +113,7 @@ RSpec.describe "Api::V1::Tasks", type: :request do
       end
     end
 
+    # 異常系：非メンバーがタスク更新
     context "as non-member" do
       it "returns forbidden" do
         task = create(:task, group: group)
@@ -115,6 +123,7 @@ RSpec.describe "Api::V1::Tasks", type: :request do
       end
     end
 
+    # 異常系：タスクが存在しない場合
     context "task does not exist" do
       it_behaves_like 'not_found', 'Task with ID' do
         before { patch "/api/v1/tasks/99999999", params: update_params, headers: headers }
@@ -127,6 +136,7 @@ RSpec.describe "Api::V1::Tasks", type: :request do
   end
 
   describe "DELETE /api/v1/tasks/:id" do
+    # 正常系：グループ管理者がタスクを削除
     context "as admin" do
       before { allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin_user) }
       it "deletes task" do
@@ -138,10 +148,12 @@ RSpec.describe "Api::V1::Tasks", type: :request do
         expect(json["message"]).to include("successfully deleted")
       end
 
+      # 異常系：存在しないタスクIDで削除
       it_behaves_like 'not_found', 'Task with ID' do
         before { delete "/api/v1/tasks/99999999", headers: headers }
       end
 
+      # 異常系：無効なタスクIDで削除
       it_behaves_like 'not_found', 'Task with ID' do
         before { delete "/api/v1/tasks/invalid_id", headers: headers }
       end
